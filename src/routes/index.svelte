04be15816616af33f2ea7c16556f08c53fb5889f
Richard Gottleber <richard@gottleber.net>
2022-08-24 10:56:46 -0500

    fix sizing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,15 +2,15 @@
 	import SVG from '$lib/SVG.svelte';
 </script>
 
-<div class="flex pt-16 p-8 flex-col xl:flex-row justify-center">
-	<div class="xl:w-3/4">
+<div class="flex pt-16 p-8 flex-col lg:flex-row justify-center">
+	<div class="lg:w-3/4">
 		<SVG />
 	</div>
-	<div class="xl:w-1/4 xl:mx-4">
-		<div class="text-center pt-4 text-4xl 2xl:text-6xl font-sans font-bold text-accent">
+	<div class="lg:w-1/4 lg:mx-4">
+		<div class="text-center pt-4 lg:pt-0 text-3xl 2xl:text-6xl font-sans font-bold text-accent">
 			The World's Most Collaborative NFT
 		</div>
-		<div class="hidden md:flex justify-center pt-8 xl:pt-16 2xl:pt-40">
+		<div class="hidden md:flex justify-center pt-8 2xl:pt-40">
 			<img src="/qr.png" alt="qr code" />
 		</div>
 		<div class="flex justify-center pt-8 xl:pt-12 2xl:pt-24 font-sans text-2xl">
